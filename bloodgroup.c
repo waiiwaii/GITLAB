f44b95998b2d50476e8da0fd855e02459daa1a57
waiiwaii <invisible1414@gmail.com>
2017-04-21 10:40:26 +0700

    Modify bloodgroup.c

--- bloodgroup.c
+++ bloodgroup.c
@@ -9,14 +9,14 @@ void main()
     scanf("%s", Name);
     do
     {
-      printf("What is your blood group [A, B, AB, or O]? ");
+      printf("What is your blood type [A, B, AB, or O]? ");
       scanf("%s", G);
 
       if (strcmp(G, "A") !=0  &&
                             strcmp(G, "B") !=0  &&
                             strcmp(G, "AB") !=0   &&
                             strcmp(G, "O") !=0 )
-        printf("Blood group %s is incorrect! Please try again.\n", G);
+        printf("Blood type %s is incorrect! Please try again.\n", G);
     }  while (strcmp(G, "A") !=0  &&
                             strcmp(G, "B") !=0  &&
                             strcmp(G, "AB") !=0   &&
